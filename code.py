2ccad7c23d4c335981c4059692cf4bc9204a2bdb
Phillip <felipe@chichasov.es>
2015-03-17 21:21:24 +0100

    Deployment 9:21 PM

--- code.py
+++ code.py
@@ -10,34 +10,78 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 """
-import flask 
-from werkzeug import secure_filename
+import web
+from os import environ
+
+
+render = web.template.render('templates/')
 
 #DATABASE SETUP
-"""
 database_url = environ['DATABASE_URL']
 database_user = environ['DATABASE_USER']
 database_pass = environ['DATABASE_PASSWORD']
 database_name = environ['DATABASE_NAME']
 database_method = 'postgres'
-db = web.database(dburl=database_url, dbn=database_method, db=database_name)
-"""
+
+db = web.database(dburl=database_url, dbn=database_method, user=database_user, pw=database_pass, db=database_name)
 ###############
 
-app = flask.Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 800 * 1024 * 1024 #Set the upload limit to 800MiB
+urls = ( 
+	'/', 'index',
+	'/v', 'watch_video',
+	'/upload', 'upload_video',
+)
+def base36encode(number, alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
+    """Converts an integer to a base36 string.
+       We will be using this for our video ID."""
+    if not isinstance(number, (int, long)):
+        raise TypeError('number must be an integer')
+    base36 = ''
+    sign = ''
+    if number < 0:
+        sign = '-'
+        number = -number
+    if 0 <= number < len(alphabet):
+        return sign + alphabet[number]
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+    return sign + base36
 
-@app.route('/')
-def index():
-	return flask.render_template('index.html')
+class index:
+	def GET(self):
+		return render.index()
 
-@app.route('/v/<videoid>')
-def viewVideo():
-	return flask.render_template('video.html')
+class watch_video:
+	def GET(self):
+		input = web.input(id=None)
+		#Sanitize input here and get data about the video
+		return render.video(str(input.id))
 
-@app.route('/upload', methods=['GET', 'POST'])
-def uploadVideo():
-	if flask.request.method == 'GET':
-		return flask.render_template('upload.html')
-	else: #POST request
-		return flask.render_template('upload.html')
+class upload_video:
+	def GET(self):
+		return render.upload()
+	def POST(self):
+		x = web.input(videoFile={}) #x is out input basket
+		filedir = "videos"
+		if 'videoFile' in x:
+			q = db.insert('videos', title=x.videoName, password=x.videoPassword, description=x.videoDescription, views=0, likes=0, dislikes=0)
+			"""
+			NOTICE
+			THE PASSWORD WILL BE ENCRYPTED, THIS IS TEMPORAL
+
+			q RETURNS THE ID
+			"""
+			videoOut = open(filedir +'/'+ base36encode(int(q)) + '.mp4','wb')
+			videoOut.write(x.videoFile.file.read())
+			videoOut.close() #upload complete
+        	raise web.seeother('/v?id=' + base36encode(int(q)))
+
+		#Video processing happens here.
+
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
+else:
+	application = web.application(urls, globals()).wsgifunc()
